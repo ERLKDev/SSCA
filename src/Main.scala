0a55d454e9f6ad8675d8bf33e378e65e0b8ad319
Erik Landkroon <eriklandkroon@gmail.com>
2017-04-07 09:40:34 +0200

    created get functions to filter result

--- src/Main.scala
+++ src/Main.scala
@@ -1,12 +1,12 @@
 import analyser.Compiler.CompilerProvider
-import analyser.util.{PrintUtil, TreeUtil}
+import analyser.util.TreeUtil
 import analyser.{Analyser, MetricRunner}
 import metrics._
 
 /**
   * Created by Erik on 5-4-2017.
   */
-object Main extends CompilerProvider with PrintUtil{
+object Main extends CompilerProvider {
   import global._
   def main (args: Array[String] ): Unit = {
     val a = "C:\\Users\\ErikL\\IdeaProjects\\SSCA\\src\\Test.scala"
@@ -15,7 +15,11 @@ object Main extends CompilerProvider with PrintUtil{
     val an = new Analyser("C:\\Users\\ErikL\\IdeaProjects\\SSCA\\src", metrics)
 
     //println(showRaw(treeFromFile(a)))
-    println(an.analyse(a))
+    val result = an.analyse()
+    val file =result.getFile("C:\\Users\\ErikL\\IdeaProjects\\SSCA\\src\\Test.scala")
+
+    file.getFun
+
  /*   c.foreach(x => println("\n"  + x))*/
 
 /*    println(showRaw(treeFromFile(a)))
---
+++ src/analyser/util/ResultUtil.scala
@@ -0,0 +1,27 @@
+package analyser.util
+
+import scala.reflect.internal.util.RangePosition
+import scala.tools.nsc.io.AbstractFile
+import scala.tools.nsc.util
+
+/**
+  * Created by ErikL on 4/7/2017.
+  */
+trait ResultUtil {
+
+  def createPosition(path: String, startLine: Int, stopLine: Int): RangePosition = {
+    def getOffsetFromLine(code: String, line: Int, incLast: Boolean): Int = {
+      val codeLines = code.split("\n").toList
+      codeLines.slice(0, line).mkString("\n").length + 1 + (if (incLast) codeLines(line).length else 0)
+    }
+
+    val absFile = AbstractFile.getFile(path)
+    val bfs = new util.BatchSourceFile(absFile, absFile.toCharArray)
+
+    val code = bfs.content.mkString
+    val startOffset = getOffsetFromLine(code, startLine - 1, false)
+    val stopOffset = getOffsetFromLine(code, stopLine, true)
+
+    new RangePosition(bfs, startOffset, startOffset, stopOffset)
+  }
+}
