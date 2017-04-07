673e66e75a1f284e622ae219d91168babd39ce94
Erik Landkroon <eriklandkroon@gmail.com>
2017-04-07 14:36:55 +0200

    small fix for loc

--- src/metrics/Loc.scala
+++ src/metrics/Loc.scala
@@ -2,6 +2,7 @@ package metrics
 
 import Utils.SourceCodeUtil
 import analyser.metric.{FunctionMetric, ObjectMetric, ProjectMetric}
+import analyser.result.UnitType.UnitType
 import analyser.result.{MetricResult, UnitType}
 
 
@@ -12,26 +13,26 @@ class Loc extends FunctionMetric with ObjectMetric with ProjectMetric with Sourc
   import global._
 
   override def run(tree: DefDef, code: List[String]): List[MetricResult] = {
-    countLocs(code, tree, getName(tree))
+    countLocs(code, tree, getName(tree), UnitType.Function)
   }
 
   override def run(tree: ModuleDef, code: List[String]): List[MetricResult] = {
-    countLocs(code, tree, getName(tree))
+    countLocs(code, tree, getName(tree), UnitType.Object)
   }
 
   override def run(tree: ClassDef, code: List[String]): List[MetricResult] = {
-    countLocs(code, tree, getName(tree))
+    countLocs(code, tree, getName(tree), UnitType.Object)
   }
 
   //TODO fix comment remove function
-  def countLocs(code: List[String], tree: Tree, name: String): List[MetricResult] = {
+  def countLocs(code: List[String], tree: Tree, name: String, uType: UnitType): List[MetricResult] = {
     val codeWithoutComments = removeComments(code)
     val codeWithComments = removeWhiteLines(code)
     val cd = (codeWithComments.size - codeWithoutComments.size).asInstanceOf[Double] / codeWithComments.size
     List(
-      MetricResult(getRangePos(tree), UnitType.Function , name, "loc", codeWithComments.size),
-      MetricResult(getRangePos(tree), UnitType.Function , name, "sloc", codeWithoutComments.size),
-      MetricResult(getRangePos(tree), UnitType.Function , name, "cd", cd))
+      MetricResult(getRangePos(tree), uType, name, "loc", codeWithComments.size),
+      MetricResult(getRangePos(tree), uType, name, "sloc", codeWithoutComments.size),
+      MetricResult(getRangePos(tree), uType, name, "cd", cd))
   }
 
 }
