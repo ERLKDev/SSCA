15f32c65f16539a7344f51f3d9afefee2280ca04
Erik Landkroon <eriklandkroon@gmail.com>
2017-05-01 16:44:16 +0200

    Added python data analyse script

---
+++ resultsAnalysis/Analysis.py
@@ -0,0 +1,185 @@
+import argparse
+import sys
+
+import pandas as pd
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+import statsmodels.api as sm
+import os
+
+class Analysis:
+
+	def __init__(self, args):
+		self.seperationLine = ''.join(["-" for _ in range(80)]) + "\n"
+		self.dependantKey = "faults"
+		self.faultTreshold = 10
+		self.args = args
+
+
+	def descriptive(self, df):
+		# Prints the descriptive statistics
+		print self.seperationLine
+		print "Descriptive statistics\n\n"
+		print df.select_dtypes(include=['float64', 'int64', 'int', 'float']).describe()
+		print "\n" + self.seperationLine
+
+
+	def correlation(self, df):
+		# Prints the correlations
+		print self.seperationLine
+		print "Correlations\n\n"
+		print df.select_dtypes(include=['float64', 'int64', 'int', 'float']).corr()
+		print "\n" + self.seperationLine
+
+	def unRegression(self, df):
+		print self.seperationLine
+		print "Univariate Regressions\n\n"
+
+		numtypes = self.getNumTypes(df)
+
+		df = df.groupby(['path']).apply(self.wavg)
+		df[self.dependantKey] = df[self.dependantKey].map(lambda x: 1 if x > self.faultTreshold else 0)
+
+		for a in numtypes:
+			
+			result = sm.OLS(df[self.dependantKey], df[a]).fit()
+			print result.summary()
+
+			result = sm.GLS(df[self.dependantKey], df[a]).fit()
+			print result.summary()
+
+			result = sm.WLS(df[self.dependantKey], df[a]).fit()
+			print result.summary()
+
+			result = sm.GLSAR(df[self.dependantKey], df[a]).fit()
+			print result.summary()
+
+			result = sm.Logit(df[self.dependantKey], df[a]).fit()
+			print result.summary()
+
+
+		print "\n" + self.seperationLine
+
+	def multiRegression(self, df):
+		print self.seperationLine
+		print "Multivariate Regressions\n\n"
+
+		numtypes = self.getNumTypes(df)
+
+		df = df.groupby(['path']).apply(self.wavg)
+		df[self.dependantKey] = df[self.dependantKey].map(lambda x: 1 if x > self.faultTreshold else 0)
+
+		result = sm.OLS(df[self.dependantKey], df[numtypes]).fit()
+		print result.summary()
+
+		result = sm.GLS(df[self.dependantKey], df[numtypes]).fit()
+		print result.summary()
+
+		result = sm.WLS(df[self.dependantKey], df[numtypes]).fit()
+		print result.summary()
+
+		result = sm.GLSAR(df[self.dependantKey], df[numtypes]).fit()
+		print result.summary()
+
+		result = sm.Logit(df[self.dependantKey], df[numtypes]).fit()
+		print result.summary()
+
+		print "\n" + self.seperationLine
+
+
+	def wavg(self, group):
+		r = []
+		for x in group.keys():
+			if str(x) == self.dependantKey:
+				r.append(group[self.dependantKey].sum())
+			elif x == "path":
+				r.append(group[x])
+			else:
+				r.append(group[x].mean())
+		return pd.Series(r, index=group.keys())
+
+
+	def distribution(self, df):
+		size = 20
+
+		for a in self.getNumTypes(df):
+			df1 = df.copy()
+			binarray = [(df1[a].max() / float(size)) * x for x in range(size + 1)]
+			df1[self.dependantKey] = df1[self.dependantKey].map(lambda x: 1 if x > self.faultTreshold else 0)
+
+			df2 = df1[(df1[self.dependantKey] > 0.0)].copy()
+
+			df2["bucket"] = pd.cut(df2[a], bins=binarray)
+			df1["bucket"] = pd.cut(df1[a], bins=binarray)
+
+			newdf = df1[['bucket',a]].groupby('bucket').count()
+			newdf2 = df2[['bucket',a]].groupby('bucket').count()
+
+			newdf3 = newdf2[a] / newdf[a]
+			newdf3.plot(kind='bar')
+			plt.title(a)
+
+			if (args.store):
+				self.storePlt("Dist-" + a)
+			else:
+				plt.show()
+
+
+	def storePlt(self, name):
+		if args.png or args.all:
+			plt.savefig(args.destination + "/" + name + '.png', format='png')
+		if args.eps or args.all:
+			plt.savefig(args.destination + "/" + name + '.eps', format='eps')
+		if args.pdf or args.all:
+			plt.savefig(args.destination + "/" + name + '.pdf', format='pdf')
+
+
+	def getNumTypes(self, df):
+		numtypes = list(df.select_dtypes(include=['float64', 'int64', 'int', 'float']).keys())
+		numtypes.remove(self.dependantKey)
+		return numtypes
+
+
+	def getStatistics(self):
+
+		df = None
+		# create a store
+		if (self.args.columns != None):
+			df = pd.concat(pd.read_csv(self.args.input, usecols=self.args.columns, chunksize=1000, iterator=True), ignore_index=True)
+		else:
+			df = pd.concat(pd.read_csv(self.args.input, chunksize=1000, iterator=True), ignore_index=True)
+		# store.append('df_key', df, complib='blosc', complevel=5)
+
+		if (self.args.store):
+			os.makedirs(self.args.destination)
+			sys.stdout = open(self.args.destination + "/output.txt", 'w')
+
+		if (not self.args.multireg):
+			self.descriptive(df)
+
+			self.correlation(df)
+			
+			self.unRegression(df)
+
+			self.distribution(df)
+
+		self.multiRegression(df)
+
+
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-i", "--input", help="The input file", dest="input", required=True)
+	parser.add_argument("-c", "--columns", help="The columns", nargs='+', dest="columns", type=int)
+	parser.add_argument("-m", "--multireg", help="Multi regression only", action="store_true", dest="multireg")
+	parser.add_argument("-s", "--store", help="Store the results", action="store_true", dest="store")
+	parser.add_argument("-d", "--destination" , help="The output path destination", dest="destination", default="./output")
+	parser.add_argument("-a", "--all" , help="Use all image types", dest="all", action="store_true")
+	parser.add_argument("--png" , help="Use image type png", dest="png", action="store_true")
+	parser.add_argument("--eps" , help="Use image type eps", dest="eps", action="store_true")
+	parser.add_argument("--pdf" , help="Use image type pdf", dest="pdf", action="store_true")
+	args = parser.parse_args()
+
+	analysis = Analysis(args)
+	analysis.getStatistics()
+	
--- resultsAnalysis/regression.py
+++ resultsAnalysis/regression.py
@@ -3,22 +3,77 @@ import pandas as pd
 import math
 import matplotlib.pyplot as plt
 import numpy as np
-from pandas.stats.api import ols
 import statsmodels.api as sm
 from sklearn import datasets, linear_model
+from scipy import stats
 
-df = pd.read_csv('fullOutputSmall.csv', usecols=[1, 2, 3, 4, 8, 9, 11, 12])
-print df.keys()
-a = "objectLOC"
+df = pd.read_csv('fullOutputO.csv')
+
+a = "WMCcc"
 b = "faults"
 
 
+# def wavg(group):
+# 	r = []
+# 	for x in group.keys():
+# 		if str(x) == b:
+# 			r.append(group[b].sum())
+# 		elif x == "path" or x == "commit":
+# 			r.append(group[x])
+# 		else:
+# 			r.append(group[x].mean())
+# 	return pd.Series(r, index=group.keys())
+
+
+# numtypes = list(df.select_dtypes(include=['float64', 'int64', 'int', 'float']).keys())
+# numtypes.remove(b)
+
+# df = df.groupby(['path']).apply(wavg)
+df[b] = df[b].map(lambda x: 1 if x > 10 else 0)
+
 # df = df.groupby(['path']).agg({b: np.sum, a: np.mean})
-# df[a] = df[a].map(lambda x: x / 100.0)
-# df[b] = df[b].map(lambda x: 1 if x > 10 else 0)
+# # df[a] = df[a].map(lambda x: x / 100.0)
+# df[b] = df[b].map(lambda x: 1 if x > 20 else 0)
 # df = df.groupby(['path'])
 # df[a] = df[a].mean()
 
+# X = df.as_matrix([a])
+# y = df.as_matrix([b])
+
+# lm = linear_model.LinearRegression()
+# lm.fit(X,y)
+# params = np.append(lm.intercept_,lm.coef_)
+# predictions = lm.predict(X)
+
+# newX = pd.DataFrame({"Constant":np.ones(len(X))}).join(pd.DataFrame(X))
+# MSE = (sum((y-predictions)**2))/(len(newX)-len(newX.columns))
+
+# # Note if you don't want to use a DataFrame replace the two lines above with
+# # newX = np.append(np.ones((len(X),1)), X, axis=1)
+# # MSE = (sum((y-predictions)**2))/(len(newX)-len(newX[0]))
+
+# var_b = MSE*(np.linalg.inv(np.dot(newX.T,newX)).diagonal())
+# sd_b = np.sqrt(var_b)
+# ts_b = params/ sd_b
+
+# p_values =[2*(1-stats.t.cdf(np.abs(i),(len(newX)-1))) for i in ts_b]
+
+# sd_b = np.round(sd_b,3)
+# ts_b = np.round(ts_b,3)
+# p_values = np.round(p_values,3)
+# params = np.round(params,4)
+
+# myDF3 = pd.DataFrame()
+# myDF3["Coefficients"],myDF3["Standard Errors"],myDF3["t values"],myDF3["Probabilites"] = [params,sd_b,ts_b,p_values]
+# print(myDF3)
+
+# plt.plot(X, predictions, color='blue', linewidth=3)
+
+# Xf1 = df.loc[df[b] == 0.0]
+# Xf2 = df.loc[df[b] > 0.0]
+# plt.scatter(Xf1[a], Xf1[b], color='black')
+# plt.scatter(Xf2[a], Xf2[b], color='red')
+# plt.show()
 
 # yY = map(lambda x: [1] if x[0] > 0 else [0], df.groupby(['path']).sum()[[b]])
 # m = np.asarray(xX)
@@ -29,50 +84,138 @@ b = "faults"
 # print df
 # print len(filter(lambda y: y[0] > 0, bB))
 
-size = int(math.ceil(len(df) *0.30))
-X = df.as_matrix([a])
-Y = df.as_matrix([b])
-X_test = df.as_matrix([a])[-size:]
-y_test = df.as_matrix([b])[-size:]
-X_train = df.as_matrix([a])[:-size]
-y_train = df.as_matrix([b])[:-size]
+# size = int(math.ceil(len(df) *0.30))
+# X = df.as_matrix([a])
+# Y = df.as_matrix([b])
+# X_test = df.as_matrix([a])[-size:]
+# y_test = df.as_matrix([b])[-size:]
+# X_train = df.as_matrix([a])[:-size]
+# y_train = df.as_matrix([b])[:-size]
 
-# df = pd.DataFrame({a :xX, b :yY})
-
-
-
-# print X_test
-# print len(X_train), len(X_test)
+# # df = pd.DataFrame({a :xX, b :yY})
 
 
-# 
-result = sm.OLS(df[b], df[a]).fit()
-print result.summary()
+
+# # print X_test
+# # print len(X_train), len(X_test)
 
 
-res = ols(y=df[b], x=df[a])
-print res
+# result = sm.OLS(df[b], df[a]).fit()
+# print result.summary()
 
-# Create linear regression object
-regr = linear_model.LinearRegression()
+# result = sm.GLS(df[b], df[a]).fit()
+# print result.summary()
 
-# Train the model using the training sets
-regr.fit(X_train, y_train)
 
-# The coefficients
-print('Coefficients: \n', regr.coef_)
-# The mean squared error
-print("Mean squared error: %f"
-      % np.mean((regr.predict(X_test) - y_test) ** 2))
-# Explained variance score: 1 is perfect prediction
-print('Variance score: %f' % regr.score(X_test, y_test))
+# result = sm.WLS(df[b], df[a]).fit()
+# print result.summary()
 
-# Plot outputs
-plt.scatter(X_test, y_test,  color='black')
-plt.plot(X_test, regr.predict(X_test), color='blue',
-         linewidth=3)
+# result = sm.GLSAR(df[b], df[a]).fit()
+# print result.summary()
 
-plt.xticks(())
-plt.yticks(())
+# result = sm.Logit(df[b], df[a]).fit()
+# print result.summary()
 
-plt.show()
+
+
+
+
+
+
+
+
+
+# a = numtypes
+res = sm.Logit(df[b], df[a]).fit()
+print res.summary()
+
+predictres = res.predict(df[a])
+
+tre = 0.5
+
+t_t = 0
+t_f = 0
+f_t = 0
+f_f = 0
+for i in range(len(df[b])):
+
+	if (bool(df[b].iloc[i]) and predictres[i] >= tre):
+		t_t += 1
+	elif (bool(df[b].iloc[i]) and predictres[i] < tre): 
+		t_f += 1
+	elif (not bool(df[b].iloc[i]) and predictres[i] >= tre):
+		f_t += 1
+	else:
+		f_f += 1
+
+print "Fault and a fault predicted = " + str(t_t)
+print "No fault and no fault predicted = " + str(f_f)
+print "Fault and a  no fault predicted = " + str(t_f)
+print "No fault and a fault predicted = " + str(f_t)
+
+# xv = np.reshape(np.arange(0.0, df[a].max(), df[a].max()/10000.0), (-1, 1))
+
+# plt.plot(df[a], df[b], 'ob', label="Data")
+# plt.plot(xv, res.predict(xv), 'or', label="OLS prediction")
+
+# plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+# print pd.crosstab(df[b], df[a], rownames=[b])
+
+# df.hist()
+# plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # Create linear regression object
+# regr = lr.LinearRegression()
+
+# # Train the model using the training sets
+# regr.fit(X_train, y_train)
+
+# # The coefficients
+# print('Coefficients: \n', regr.coef_)
+# print('Coefficients: \n', regr.p)
+
+# # The mean squared error
+# print("Mean squared error: %f"
+#       % np.mean((regr.predict(X_test) - y_test) ** 2))
+# # Explained variance score: 1 is perfect prediction
+# print('Variance score: %f' % regr.score(X_test, y_test))
+
+# # Plot outputs
+# plt.scatter(X_test, y_test,  color='black')
+# plt.plot(X_test, regr.predict(X_test), color='blue',
+#          linewidth=3)
+
+# plt.xticks(())
+# plt.yticks(())
+# plt.savefig('destination_path.eps', format='eps')
+# plt.savefig('destination_path.pdf', format='pdf')
+# plt.savefig('destination_path.png', format='png')
+# plt.show()
