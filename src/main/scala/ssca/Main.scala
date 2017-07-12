e3c9ac4d0d7946e7f722cff1562ebf36a8739bfa
Erik Landkroon <eriklandkroon@gmail.com>
2017-07-12 16:22:28 +0200

    Faster run

--- src/main/scala/ssca/Main.scala
+++ src/main/scala/ssca/Main.scala
@@ -3,7 +3,6 @@ package ssca
 import codeAnalysis.STimer
 import codeAnalysis.metrics.{PatternSize, _}
 import dispatch.Http
-import main.scala.analyser.metric.Metric
 import ssca.validator._
 
 
@@ -11,22 +10,6 @@ import ssca.validator._
   * Created by Erik on 13-4-2017.
   */
 object Main {
-
-  val dataSets : List[(String, List[Metric])] = List(
-    ("Obj", List(new DIT, new LCOM, new RFC, new WMC, new NOC, new CBO)),
-    ("ObjG", List(new DIT, new LCOM, new RFC, new WMC, new NOC, new CBO, new Loc, new Complex)),
-    ("Func", List(new PatternSize, new OutDegree, new NPVS, new DON)),
-    ("FuncG", List(new PatternSize, new OutDegree, new NPVS, new DON, new Loc, new Complex)),
-    ("FuncS", List(new PatternSize, new OutDegree, new NPVS, new DON, new FunctionalMetrics)),
-    ("FuncGS", List(new PatternSize, new OutDegree, new NPVS, new DON, new FunctionalMetrics, new Loc, new Complex)),
-    ("Gen", List(new Loc, new Complex)),
-    ("GenS", List(new Loc, new Complex, new FunctionalMetrics)),
-    ("Comb", List(new DIT, new LCOM, new RFC, new WMC, new NOC, new CBO, new PatternSize, new OutDegree, new NPVS, new DON)),
-    ("CombG", List(new DIT, new LCOM, new RFC, new WMC, new NOC, new CBO, new PatternSize, new OutDegree, new NPVS, new DON, new Loc, new Complex)),
-    ("CombS", List(new DIT, new LCOM, new RFC, new WMC, new NOC, new CBO, new PatternSize, new OutDegree, new NPVS, new DON, new FunctionalMetrics)),
-    ("CombGS", List(new DIT, new LCOM, new RFC, new WMC, new NOC, new CBO, new PatternSize, new OutDegree, new NPVS, new DON, new Loc, new Complex, new FunctionalMetrics))
-  )
-
 
   def main(args: Array[String]): Unit = {
     val repoUser = "shadowsocks"
@@ -37,16 +20,13 @@ object Main {
     val branch = "master"
     val labels = List("bug")
 
-    dataSets.foreach{
-      x =>
-        val validator = new ValidatorNObject(repoPath, repoUser, repoName, branch, labels, 3, 5, x._2, "fullOutput" + project + version + "New" + x._1)
-        STimer.time("Analysis", validator.run())
-    }
-    dataSets.foreach{
-      x =>
-        val validator = new ValidatorOObject(repoPath, repoUser, repoName, branch, labels, 3, 5, x._2, "fullOutput" + project + version + "Old" + x._1)
-        STimer.time("Analysis", validator.run())
-    }
+    val metrics = List(new DIT, new LCOM, new RFC, new WMC, new NOC, new CBO, new PatternSize, new OutDegree, new NPVS, new DON, new Loc, new Complex, new FunctionalMetrics)
+
+    val validatorN = new ValidatorNObject(repoPath, repoUser, repoName, branch, labels, 3, 5, metrics, "fullOutput" + project + version + "New")
+    STimer.time("Analysis", validatorN.run())
+
+    val validatorO = new ValidatorOObject(repoPath, repoUser, repoName, branch, labels, 3, 5, metrics, "fullOutput" + project + version + "Old")
+    STimer.time("Analysis", validatorO.run())
     Http.shutdown()
   }
 }
